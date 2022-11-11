c5c96dc0ce6d84a5dca18be21633fbdbbcd3b719
smart-programmer <albrns1123488@outlook.com>
2022-11-11 15:16:42 +0300

    change colors

--- public/apartment_detail_view.php
+++ public/apartment_detail_view.php
@@ -90,7 +90,7 @@
 
                             <!-- Modal -->
                             <div class="modal fade" id="rating-modal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="rating-modal-label" aria-hidden="true">
-                                <div class="modal-dialog modal-dialog-centered modal-fullscreen-sm-down">
+                                <div class="modal-dialog modal-dialog-centered">
                                     <div class="modal-content">
                                         <div class="modal-header">
                                             <h1 class="modal-title fs-5 rating-modal-label" id="rating-modal-label">Rate this apartment</h1>
