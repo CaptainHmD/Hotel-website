1ef6ca7b20f6d078db708e1efb05276eece5a06e
HaMaD <hamad.laaboun@gmail.com>
2022-11-13 01:28:59 +0300

    added login layout

--- public/js/script.js
+++ public/js/script.js
@@ -2,15 +2,19 @@ const textAnimation = document.querySelector('#text-animation');
 
 let textCounter = 0
 const prettyPhrases = ['Deluxe Suite~',"Double Room~","Single Room~"]
-textAnimation.addEventListener('animationiteration', () => {
+
+
+try {
+  textAnimation.addEventListener('animationiteration', () => {
     if(prettyPhrases.length===textCounter)
     textCounter=0
     
     textAnimation.dataset.text  = prettyPhrases[textCounter]
     textCounter++
 });
-
-
+} catch (error) {
+  
+}
 
 // When the user scrolls down 20px from the top of the document, slide down the navbar and change alpha to 1
 // When the user scrolls to the top of the page, slide up the navbar (default = 10%) and return alpha to 0.7
@@ -30,10 +34,6 @@ function scrollFunction() {
 
 
 
-// // bootstrap modal
-// const myModal = document.getElementById('myModal')
-// const myInput = document.getElementById('myInput')
 
-// // myModal.addEventListener('shown.bs.modal', () => {
-// //   myInput.focus()
-// // })
+
+
